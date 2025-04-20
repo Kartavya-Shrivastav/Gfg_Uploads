fc56c2fb46662681a9abff56cd5468466d7eefbc
Kartavya-Shrivastav <kartavyashrivastav28@gmail.com>
2025-04-20 23:33:56 +0530

    Made some changes in the code

--- Day_118_Find_Only_Repetitive_Element_from_1_to_n-1.cpp
+++ Day_118_Find_Only_Repetitive_Element_from_1_to_n-1.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 class Solution {
-    public int findDuplicate(int[] arr) {
-        int len=arr.length;
-        int ans=arr[0];
-        for(int i=1;i<len;i++){
-            ans^=arr[i]^i;
-        }
-        return ans;
+    public:
+        int findDuplicate(vector<int> arr) {
+            int len = arr.size();
+            int ans=arr[0];
+            for(int i=1;i<len;i++){
+                ans^=arr[i]^i;
+            }
+            return ans;
     }
-}
+};
