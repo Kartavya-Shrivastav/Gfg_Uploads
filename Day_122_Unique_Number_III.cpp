e7c43b9bf8fa889117516d31704fcd28be7e30ed
Kartavya-Shrivastav <kartavyashrivastav28@gmail.com>
2025-04-24 23:22:00 +0530

    Solved using bitwise state tracking to find the element appearing only once when all others appear thrice.

---
+++ Day_122_Unique_Number_III.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int getSingle(vector<int> &arr) {
+        // code here
+        int ones(0), twos(0);
+        for(auto it: arr) {
+            ones = (ones ^ it) & ~twos;
+            twos = (twos ^ it) & ~ones;
+        }
+        return ones;
+    }
+};
