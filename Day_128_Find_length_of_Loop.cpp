fb39ab5ae1cad23ef25fd91160b115933cb139c2
Kartavya-Shrivastav <kartavyashrivastav28@gmail.com>
2025-04-30 23:35:46 +0530

    Refactor Node structure and update countNodesinLoop function to improve clarity and consistency

--- Day_128_Find_length_of_Loop.cpp
+++ Day_128_Find_length_of_Loop.cpp
@@ -1,23 +1,43 @@
 
-class Node
-{
+struct Node {
     int data;
-    Node next;
-    Node(int d) {data = d; next = null; }
-}
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
 
-class Solution {
+class Solution
+{
+public:
+    int findLength(Node *slow, Node *fast)
+    {
+
+        int count = 1;
+        fast = fast->next;
+        while (fast != slow)
+        {
+            count++;
+            fast = fast->next;
+        }
+        return count;
+    }
     // Function to find the length of a loop in the linked list.
-    public int countNodesinLoop(Node head) {
-        Map<Node,Integer> hm=new HashMap<>();
-        int len=1;
-        Node curr=head;
-        while(curr!=null){
-            if(hm.containsKey(curr)) return len-hm.get(curr);
-            hm.put(curr,len);
-            len++;
-            curr=curr.next;
+    int countNodesinLoop(Node *head)
+    {
+        Node *slow = head;
+        Node *fast = head;
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+            {
+                return findLength(slow, fast);
+            }
         }
         return 0;
     }
-}
+};
