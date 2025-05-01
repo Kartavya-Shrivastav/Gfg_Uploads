e7f3afc25febeeb404f51ffb0d34c00abf739f47
Kartavya-Shrivastav <kartavyashrivastav28@gmail.com>
2025-05-01 23:09:48 +0530

    Refactor nthRowOfPascalTriangle function to improve clarity and consistency

--- Day_129_Pascal_Triangle.cpp
+++ Day_129_Pascal_Triangle.cpp
@@ -1,19 +1,39 @@
+class Solution
+{
+public:
+    vector<int> nthRowOfPascalTriangle(int n)
+    {
+        if (n == 1)
+            return {1};
 
-class Solution {
-
-    ArrayList<Integer> nthRowOfPascalTriangle(int n) {
-        // code here
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(1);
-        for (int i = 1; i < n; i++) {
-            ArrayList<Integer> temp = new ArrayList<>();
-            temp.add(1);
-            for (int j = 1; j < arr.size(); j++) {
-                temp.add(arr.get(j - 1) + arr.get(j));
+        vector<vector<int>> triangle;
+        triangle.push_back({1});
+        for (int i = 1; i < n - 1; i++)
+        {
+            vector<int> curr;
+            for (int j = 0; j <= i; j++)
+            {
+                if (j == 0 || j == i)
+                {
+                    curr.push_back(1);
+                }
+                else
+                {
+                    curr.push_back(triangle[i - 1][j - 1] + triangle[i - 1][j]);
+                }
             }
-            temp.add(1);
-            arr = temp;
+            triangle.push_back(curr);
         }
-        return arr;
+        vector<int> curr;
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0 || i == n - 1)
+            {
+                curr.push_back(1);
+            }
+            else
+                curr.push_back(triangle[n - 2][i - 1] + triangle[n - 2][i]);
+        }
+        return curr;
     }
-}
+};
