e629d5a9db7b8c96094347f9f6c43db8ac08cab9
Kartavya-Shrivastav <kartavyashrivastav28@gmail.com>
2025-05-11 22:27:08 +0530

    Add implementation of kthLargest function to find the k-th largest sum of contiguous subarrays

---
+++ Day_139_K_th_Largest_Sum_Contiguous_Subarray.cpp
@@ -0,0 +1,26 @@
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int kthLargest(vector<int> &arr, int k) {
+        // code here
+         priority_queue<int,vector<int>,greater<int>>pq;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=arr[j];
+                if(pq.size()==k){
+                    if(pq.top()<sum){
+                        pq.pop();
+                        pq.push(sum);
+                    }
+                }else{
+                    pq.push(sum);
+                }
+            }
+        }
+        return pq.top();
+    }
+};
