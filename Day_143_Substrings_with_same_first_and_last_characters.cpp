37f3226d5f12e5e439896a09df5a97882d5d9028
Kartavya-Shrivastav <kartavyashrivastav28@gmail.com>
2025-05-15 23:03:46 +0530

    Refactor access modifier indentation in Solution class for consistency

--- Day_143_Substrings_with_same_first_and_last_characters.cpp
+++ Day_143_Substrings_with_same_first_and_last_characters.cpp
@@ -1,23 +1,17 @@
 
 class Solution {
-  public:
+public:
     int countSubstring(string &s) {
-        // code here
-         int n = s.length();
-        
+        int n = s.length();
         vector<int> freq(26);
-        
         int cnt = 0;
-        
         for(int i = 0; i<n; i++) {
             freq[s[i]-'a']++;
         }
-        
         for(int c : freq) {
             if(c > 0)
                 cnt += (((c)*(c-1))/(2));
         }
-        
         return (cnt + n);
     }
 };
