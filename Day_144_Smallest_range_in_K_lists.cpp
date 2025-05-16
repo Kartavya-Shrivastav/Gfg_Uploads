90a1cb2b558389648bd5477ff2498400a443c148
Kartavya-Shrivastav <kartavyashrivastav28@gmail.com>
2025-05-16 23:41:55 +0530

    Refactor access modifier alignment and remove commented code in findSmallestRange function

--- Day_144_Smallest_range_in_K_lists.cpp
+++ Day_144_Smallest_range_in_K_lists.cpp
@@ -1,9 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
 
 class Solution {
-  public:
+public:
     vector<int> findSmallestRange(vector<vector<int>>& arr) {
-        // code here
-         map<int,int>poin;
+        map<int,int>poin;
         int maxi=0;
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>minH;
         int minRange=INT_MAX;
@@ -22,8 +23,8 @@ class Solution {
                 ans={ele.first,maxi};
             }
             poin[ele.second]++;
-             if (poin[ele.second] == arr[ele.second].size()) {
-            break;  
+            if (poin[ele.second] == arr[ele.second].size()) {
+            break;
         }
             minH.push({arr[ele.second][poin[ele.second]],ele.second});
             maxi=max(maxi,arr[ele.second][poin[ele.second]]);
