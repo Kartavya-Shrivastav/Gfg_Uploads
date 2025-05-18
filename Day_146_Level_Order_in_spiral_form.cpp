cb977d0a191db3ca759615a1eff62febb0e2c2b1
Kartavya-Shrivastav <kartavyashrivastav28@gmail.com>
2025-05-18 23:44:25 +0530

    Refactor findSpiral function for improved readability and maintainability

--- Day_146_Level_Order_in_spiral_form.cpp
+++ Day_146_Level_Order_in_spiral_form.cpp
@@ -1,3 +1,5 @@
+#include<bits/stdc++.h>
+using namespace std;
 
 /* A binary tree node has data, pointer to left child
     and a pointer to right child
@@ -14,45 +16,44 @@ class Node
 }; */
 class Solution {
     public:
-      vector<int> findSpiral(Node* root) {
-          // code here
-          vector<int> ans;
-          
-          queue<Node*> q;
-          bool flag = false;
-          q.push(root);
-          stack<int> st;
-          while(!q.empty()){
-              int n = q.size();
+    vector<int> findSpiral(Node* root) {
+        vector<int> ans;
         
-              for(int i = 0 ; i < n ; i++){
-                  Node* node = q.front();
-                  
-                  q.pop();
-                  
-                 if(flag) ans.push_back(node -> data);
-                 
-                 else{
-                     st.push(node -> data);
-                 }
-                  
-                  if(node -> left){
-                      q.push(node -> left);
-                  }
-                  
-                  if(node -> right){
-                      q.push(node -> right);
-                  }
-              }
-              
-              while(!st.empty()){
-                  ans.push_back(st.top());
-                  st.pop();
-              }
-              flag = !flag;
-          }
-          
-          
-          return ans;
-      }
-  };
+        queue<Node*> q;
+        bool flag = false;
+        q.push(root);
+        stack<int> st;
+        while(!q.empty()){
+            int n = q.size();
+    
+            for(int i = 0 ; i < n ; i++){
+                Node* node = q.front();
+                
+                q.pop();
+                
+                if(flag) ans.push_back(node -> data);
+                
+                else{
+                    st.push(node -> data);
+                }
+                
+                if(node -> left){
+                    q.push(node -> left);
+                }
+                
+                if(node -> right){
+                    q.push(node -> right);
+                }
+            }
+            
+            while(!st.empty()){
+                ans.push_back(st.top());
+                st.pop();
+            }
+            flag = !flag;
+        }
+        
+        
+        return ans;
+    }
+};
