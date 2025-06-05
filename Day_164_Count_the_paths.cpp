430e185462f17111a6077ff5202ef8e31b546086
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-05 13:45:54 +0530

    Refactor dfs and countPaths functions for improved readability and clarity in comments

--- Day_164_Count_the_paths.cpp
+++ Day_164_Count_the_paths.cpp
@@ -3,29 +3,37 @@ using namespace std;
 // This code defines a Solution class with a method to count the number of paths from a source vertex to a destination vertex in a directed graph represented by edges.
 
 class Solution {
-  public:
-    int dfs(int curr, int dest, vector<vector <int>>& adj, vector <int>& vis){
-        if(curr == dest) return 1;
-        if(vis[curr] != -1) return vis[curr];
-        
-        int cnt = 0;
-        for(int nbr: adj[curr]){
-            cnt += dfs(nbr, dest, adj, vis);
+    public:
+        // Helper function to perform DFS and count paths from curr to dest
+        int dfs(int curr, int dest, vector<vector <int>>& adj, vector <int>& vis){
+                // If current node is the destination, one path is found
+                if(curr == dest) return 1;
+                // If already computed, return memoized result
+                if(vis[curr] != -1) return vis[curr];
+                
+                int cnt = 0;
+                // Explore all neighbors
+                for(int nbr: adj[curr]){
+                        cnt += dfs(nbr, dest, adj, vis);
+                }
+                
+                // Memoize and return the count
+                return vis[curr] = cnt;
         }
-        
-        return vis[curr] = cnt;
-    }
-    int countPaths(vector<vector<int>>& edges, int V, int src, int dest) {
-        vector<vector <int>> adj(V);
-        vector <int> vis(V, -1);
-        
-        for(auto edge: edges){
-            int u = edge[0];
-            int v = edge[1];
-            
-            adj[u].push_back(v);
+
+        // Main function to count number of paths from src to dest
+        int countPaths(vector<vector<int>>& edges, int V, int src, int dest) {
+                vector<vector <int>> adj(V); // Adjacency list
+                vector <int> vis(V, -1);     // Memoization array
+                
+                // Build the adjacency list from edge list
+                for(auto edge: edges){
+                        int u = edge[0];
+                        int v = edge[1];
+                        adj[u].push_back(v);
+                }
+                
+                // Start DFS from source
+                return dfs(src, dest, adj, vis);
         }
-        
-        return dfs(src, dest, adj, vis);
-    }
 };
