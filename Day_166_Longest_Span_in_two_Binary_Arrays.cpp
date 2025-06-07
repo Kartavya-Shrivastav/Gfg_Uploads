fd80e177b4379925695ff8e360b0f924505c7b2e
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-07 23:32:05 +0530

    Refactor longestCommonSum function for improved readability and clarity in comments

--- Day_166_Longest_Span_in_two_Binary_Arrays.cpp
+++ Day_166_Longest_Span_in_two_Binary_Arrays.cpp
@@ -4,23 +4,27 @@ using namespace std;
 // This code defines a Solution class with a method to find the longest span with equal sum in two binary arrays.
 
 class Solution {
-  public:
-    int longestCommonSum(vector<int> &a1, vector<int> &a2) {
-        // Code here.
-         int n=a1.size();
-        unordered_map<int, int>mpp;
-        int sum=0;
-        int maxi=0;
-        
-        mpp[0]=-1;
-        
-        for(int i=0; i<n; i++){
-            sum+=(a1[i]-a2[i]);
-            if(mpp.find(sum)!=mpp.end()){
-                maxi=max(maxi, i-mpp[sum]);
-            }else mpp[sum]=i;
+    public:
+        // Function to find the longest span with equal sum in two binary arrays
+        int longestCommonSum(vector<int> &a1, vector<int> &a2) {
+                int n = a1.size();
+                unordered_map<int, int> mpp; // Map to store the first occurrence of a particular sum difference
+                int sum = 0; // This will store the running sum of differences
+                int maxi = 0; // To keep track of the maximum length found
+                
+                mpp[0] = -1; // Initialize with sum 0 at index -1 to handle the case when the span starts from index 0
+                
+                for (int i = 0; i < n; i++) {
+                        sum += (a1[i] - a2[i]); // Update the running sum of differences
+                        if (mpp.find(sum) != mpp.end()) {
+                                // If this sum has been seen before, update the maximum length
+                                maxi = max(maxi, i - mpp[sum]);
+                        } else {
+                                // Otherwise, store the index of the first occurrence of this sum
+                                mpp[sum] = i;
+                        }
+                }
+                
+                return maxi; // Return the maximum length found
         }
-        
-        return maxi;
-    }
 };
