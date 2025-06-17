df8c9364c067f39d3411f6bb9c5542dd575bb93b
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-17 16:50:03 +0530

    Refactor findMaxGuests function for improved readability and maintainability

--- Maximum_Intervals_Overlap.cpp
+++ Maximum_Intervals_Overlap.cpp
@@ -1,17 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem: Find the maximum number of guests present at any time in a party given entry and exit times.
+// Approach: Use a map to track the number of guests entering and exiting at each time point.
+
 class Solution {
-
-  public:
-    vector<int> findMaxGuests(int Entry[], int Exit[], int N) {
-        // Your code goes here
-        map<int, int> mp;
-	    for(int i = 0; i < N; i++) mp[Entry[i]]++, mp[Exit[i]+1]--;
-	    int cnt = 0, max_cnt = 0;
-	    vector<int> ret(2);
-        for(auto &x : mp){
-            cnt += x.second;
-            if(cnt > max_cnt) max_cnt = cnt, ret[0] = max_cnt, ret[1] = x.first; 
+    public:
+        vector<int> findMaxGuests(int Entry[], int Exit[], int N) {
+                // Map to store the net change in guest count at each time point
+                map<int, int> mp;
+                // Increment at entry time, decrement at exit+1 time
+                for(int i = 0; i < N; i++) {
+                        mp[Entry[i]]++;      // Guest enters
+                        mp[Exit[i]+1]--;     // Guest leaves after exit time
+                }
+                int cnt = 0;             // Current number of guests
+                int max_cnt = 0;         // Maximum number of guests at any time
+                vector<int> ret(2);      // ret[0]: max guests, ret[1]: time of max guests
+                // Traverse the map in time order
+                for(auto &x : mp){
+                        cnt += x.second;     // Update current guest count
+                        if(cnt > max_cnt) {  // Check if current count is the new maximum
+                                max_cnt = cnt;
+                                ret[0] = max_cnt;
+                                ret[1] = x.first; // Record the time of max guests
+                        }
+                }
+                return ret;              // Return max guests and the time it occurred
         }
-        return ret;
-
-    }
 };
