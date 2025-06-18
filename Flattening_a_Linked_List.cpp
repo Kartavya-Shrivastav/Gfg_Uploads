14c2a00565259074bbaf35db420a82d9bdbaeb06
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-18 10:47:32 +0530

    Implement flatten function to merge multiple linked lists into a single sorted list

---
+++ Flattening_a_Linked_List.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+  public:
+    Node *flatten(Node *root){
+        priority_queue<pair<int, Node*> , vector<pair<int, Node*>> , greater<pair<int, Node*>>> pq;
+        Node* temp= root;
+        while(temp){
+            pq.push(make_pair(temp->data, temp));
+            temp= temp->next;
+        }
+        Node* dummy= new Node(-1);
+        Node* tail= dummy;
+        while(!pq.empty()){
+            pair<int, Node*> temp= pq.top();
+            pq.pop();
+            Node* node= temp.second;
+            tail->bottom= node;
+            if(temp.second->bottom){
+                pq.push(make_pair(temp.second->bottom->data, temp.second->bottom));
+                
+            }
+            tail= node;
+        }
+        return dummy->bottom;
+    }
+};
