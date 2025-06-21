3a93727a45f6715a24535f310472f231c5eba980
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-21 23:38:21 +0530

    Refactor catchThieves function in Day_180_Police_and_Thieves.cpp for improved readability and clarity of comments

--- Day_180_Police_and_Thieves.cpp
+++ Day_180_Police_and_Thieves.cpp
@@ -14,34 +14,41 @@ using namespace std;
 // Space Complexity: O(n) for storing indices of police and thieves.
 
 class Solution {
-  public:
-    int catchThieves(vector<char> &arr, int k) {
-        // Code here
-        vector<int>p, t;
-        for(int i=0; i<arr.size(); i++){
-            if(arr[i] == 'P') p.push_back(i);
-            else t.push_back(i);
+    public:
+        int catchThieves(vector<char> &arr, int k) {
+                // Vectors to store indices of police and thieves
+                vector<int> p, t;
+                for(int i = 0; i < arr.size(); i++) {
+                        if(arr[i] == 'P')
+                                p.push_back(i); // Store police index
+                        else
+                                t.push_back(i); // Store thief index
+                }
+                
+                int ans = 0; // Count of caught thieves
+                int st = 0;  // Pointer for police vector
+                // Iterate through each thief
+                for(int i = 0; i < t.size(); i++) {
+                        // Try to find a police for the current thief
+                        while(st < p.size()) {
+                                int low = t[i] - k;   // Minimum index police can be at
+                                int high = t[i] + k;  // Maximum index police can be at
+                                if(p[st] <= high && p[st] >= low) {
+                                        // Police is within range, catch the thief
+                                        ans++;
+                                        st++; // Move to next police
+                                        break;
+                                }
+                                else if(p[st] < low) {
+                                        // Police is too far left, move to next police
+                                        st++;
+                                }
+                                else {
+                                        // Police is too far right, can't catch this thief
+                                        break;
+                                }
+                        }
+                }
+                return ans; // Return total caught thieves
         }
-        
-        int ans  = 0;
-        int st = 0;
-        for(int i=0; i<t.size(); i++){
-            while(st < p.size()){
-                int low = t[i] - k;
-                int high = t[i] + k;
-                if(p[st] <= high && p[st] >= low){
-                    ans++;
-                    st++;
-                    break;
-                }
-                else if(p[st] < low){
-                    st++;
-                }
-                else{
-                    break;
-                }
-            }
-        }
-        return ans;
-    }
 };
