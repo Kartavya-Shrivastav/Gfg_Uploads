c1ad85cb7e0eec3f6825cc532bdd7ab062a85692
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-28 22:58:14 +0530

    Add solution for Kth Largest in a Stream problem

---
+++ Kth_Largest_in_a_Stream.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<int> kthLargest(int k, int arr[], int n) {
+        vector<int> res;
+        priority_queue<int, vector<int>, greater<int>>pq;
+        for(int i=0;i<n;i++){
+            pq.push(arr[i]);
+            if(pq.size()>k){
+                pq.pop();
+            }
+            if(pq.size()==k){
+                res.push_back(pq.top());
+            }else{
+                res.push_back(-1);
+            }
+        }
+        return res;
+    }
+};
