59c617bda0a595424a4cb7056a564c2735a93afb
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-07-23 22:15:08 +0530

    Add function to increment an integer represented as a digit array, handling carry and overflow.

---
+++ Plus_One.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> increment(vector<int> arr, int N) {
+        for (int i = N - 1; i >= 0; --i) {
+            if (arr[i] < 9) {
+                arr[i]++;
+                return arr;
+            } else {
+                arr[i] = 0;
+            }
+        }
+        vector<int> result(N + 1);
+        result[0] = 1;
+        return result;
+    }
+};
