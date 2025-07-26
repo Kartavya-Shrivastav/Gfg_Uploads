fa3be9cd65cb78854e0f1bc8556b61a75dc8ac6f
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-07-26 09:22:02 +0530

    Added hash map-based solution to find all elements appearing more than n/3 times in the array.

---
+++ Day_215_Majority_Element_II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> findMajority(vector<int>& arr) {
+        unordered_map<int, int> mp;
+        for (int& it : arr){
+            mp[it]++;
+        }
+        int n = arr.size();
+        vector<int> ans;
+        for (auto& it : mp){
+            if (it.second > n/3) ans.push_back(it.first);
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
