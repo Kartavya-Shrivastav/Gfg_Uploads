437436f4fa4b6b71226de53803e5f0b27f72babe
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-09-03 23:06:48 +0530

    feat: implement reverse function to reverse a doubly linked list

---
+++ Day_254_Reverse_a_Doubly_Linked_List.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+  public:
+    Node *reverse(Node *head) {
+        Node * temp=head;
+        while (temp){
+            Node * node = temp->next;
+            temp->next = temp->prev;
+            temp->prev = node;
+            if(!node) break;
+            temp = node;
+        }
+        
+        return temp;
+    }
+};
