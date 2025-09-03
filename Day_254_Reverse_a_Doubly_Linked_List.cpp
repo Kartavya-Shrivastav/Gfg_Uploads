2aa3ac77642583f96b60c1cd43f005d13c095d48
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-09-03 23:07:00 +0530

    style: remove redundant Node class definition from reverse function implementation

--- Day_254_Reverse_a_Doubly_Linked_List.cpp
+++ Day_254_Reverse_a_Doubly_Linked_List.cpp
@@ -1,4 +1,17 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int val) {
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
 
+*/
 class Solution {
   public:
     Node *reverse(Node *head) {
