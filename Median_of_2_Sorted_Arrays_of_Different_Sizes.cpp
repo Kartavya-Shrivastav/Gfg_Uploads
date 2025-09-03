4b1ec1c3c1913deccdb731af8627deb901a8aa59
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-09-03 23:14:02 +0530

    feat: implement function to calculate the median of two sorted arrays

---
+++ Median_of_2_Sorted_Arrays_of_Different_Sizes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    double medianOf2(vector<int>& a, vector<int>& b) {
+        vector<int> v;
+        for(auto it:a){
+            v.push_back(it);
+        }
+        for(auto it:b){
+            v.push_back(it);
+        }
+        sort(v.begin(),v.end());
+
+        int n = v.size();
+        return n%2!=0?v[(n)/2]:(v[n/2]+v[(n/2)-1])/2.0;
+    }
+};
