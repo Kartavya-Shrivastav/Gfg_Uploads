0977fd2f53516d6d43f25b5506dd1ad269417ddf
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-09-04 23:21:27 +0530

    feat: implement reverseKGroup function to reverse nodes in k-sized groups

---
+++ Day_255_Linked_List_Group_Reverse.cpp
@@ -0,0 +1,43 @@
+
+/*
+  Node is defined as
+    struct node
+    {
+        int data;
+        struct Node* next;
+
+        Node(int x){
+            data = x;
+            next = NULL;
+        }
+
+    }*head;
+*/
+
+class Solution {
+  public:
+    Node *reverseKGroup(Node *head, int k) {
+        // code here
+        Node* prev = NULL;
+        Node* curr = head;
+        Node* next = NULL;
+        int count = 0;
+        while(curr != NULL && count < k){
+            next = curr->next;
+            curr->next = prev;
+            
+            prev = curr;
+            curr = next;
+            count++;
+        }
+        
+        if(next != NULL){
+            head->next = reverseKGroup(next, k);
+        }
+        
+        return prev;
+        
+        
+        
+    }
+};
