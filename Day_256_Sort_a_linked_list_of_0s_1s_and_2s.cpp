ba585b1a0da47f1d84db4d0c6d0fa4ef43c5fead
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-09-05 23:27:10 +0530

    style: add definition of Node struct in comments for clarity

--- Day_256_Sort_a_linked_list_of_0s_1s_and_2s.cpp
+++ Day_256_Sort_a_linked_list_of_0s_1s_and_2s.cpp
@@ -1,4 +1,14 @@
 
+/*  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};*/
+
 class Solution {
   public:
    Node* segregate(Node* h) {
