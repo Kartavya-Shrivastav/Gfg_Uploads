4fac3da3ec87f2153f68baf74f38507daaa13bab
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-09-05 23:27:37 +0530

    style: enhance comments and documentation for the sorting approach of linked list

--- Day_256_Sort_a_linked_list_of_0s_1s_and_2s.cpp
+++ Day_256_Sort_a_linked_list_of_0s_1s_and_2s.cpp
@@ -1,3 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem: Sort a linked list of 0s, 1s and 2s
+// Approach: Use a multiset to store the values and then overwrite the linked list
+
+// Steps:
+// 1. Traverse the linked list and store the values in a multiset.
+// 2. Traverse the linked list again and overwrite the values from the multiset.
+// 3. Return the head of the modified linked list.
+
+// Time Complexity: O(N log N) due to multiset insertion
+// Space Complexity: O(N) for storing values in the multiset
+
 
 /*  Node is defined as
   struct Node {
