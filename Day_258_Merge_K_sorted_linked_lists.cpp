285037b148da38c8288d65f069ba68624fef76f0
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-09-07 23:40:04 +0530

    feat: implement mergeTwo and mergeKLists functions for merging sorted linked lists

---
+++ Day_258_Merge_K_sorted_linked_lists.cpp
@@ -0,0 +1,41 @@
+
+
+class Solution {
+  public:
+  
+    Node* mergeTwo(Node* head1, Node* head2){
+        Node* dummyHead = new Node(-1);
+        Node* curr = dummyHead;
+        
+        while(head1 != NULL && head2 != NULL){
+            if(head1->data <= head2->data){
+                curr->next = head1;
+                head1 = head1->next;
+            }
+            else{
+                curr->next = head2;
+                head2 = head2->next;
+            }
+            
+            curr = curr->next;
+        }
+        
+        if(head1 != NULL){
+            curr->next = head1;
+        }
+        else{
+            curr->next = head2;
+        }
+        
+        return dummyHead->next;
+    }
+  
+    Node* mergeKLists(vector<Node*>& arr) {
+        Node* res = NULL;
+        for(Node* node : arr){
+            res = mergeTwo(res, node);
+        }
+        
+        return res;
+    }
+};
