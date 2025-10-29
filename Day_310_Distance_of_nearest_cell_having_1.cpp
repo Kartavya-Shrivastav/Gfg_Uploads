83469f4184c2d96fc329648b9ee63a76aeb19d47
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-10-29 09:08:27 +0530

    refactor: optimize nearest function for calculating distances in grid

--- Day_310_Distance_of_nearest_cell_having_1.cpp
+++ Day_310_Distance_of_nearest_cell_having_1.cpp
@@ -1,63 +1,37 @@
+
 class Solution {
   public:
+    // Function to find distance of nearest 1 in the grid for each cell.
     vector<vector<int>> nearest(vector<vector<int>>& grid) {
-        int x=grid.size();
-        int y=grid[0].size();
-        vector<vector<int>>visited(x,vector<int>(y,0));
-         vector<vector<int>>final(x,vector<int>(y,0));
-         deque<pair<pair<int,int>,int>>q;
-         
-        for(int i=0;i<x;i++){
-            for(int j=0;j<y;j++){
-                if(grid[i][j]==1){
-                    visited[i][j]=1;
-                    q.push_back({{i,j},0});
+        // Code here
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dist(m, vector<int>(n,0));
+        vector<vector<int>> vis(m,vector<int>(n,0));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i = 0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1){
+                    q.push({{i,j},0});
+                    vis[i][j] = 1;
                 }
             }
         }
-        
         while(!q.empty()){
-            auto it=q.front();
-            q.pop_front();
-            if(it.second!=0){
-                final[it.first.first][it.first.second]=it.second;
-            }
-            
-            if(it.first.first-1>=0){
-                if(visited[it.first.first-1][it.first.second]==0){
-                    q.push_back({{it.first.first-1,it.first.second},it.second+1});
-                    visited[it.first.first-1][it.first.second]=1;
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            int steps = q.front().second;
+            q.pop();
+            dist[x][y] = steps;
+            vector<pair<int,int>> dir = {{0,1},{1,0},{0,-1},{-1,0}};
+            for(auto d: dir){
+                int nx = x + d.first, ny = y + d.second;
+                if(nx>=0 && nx<m && ny>=0 && ny<n && vis[nx][ny] == 0){
+                    vis[nx][ny] = 1;
+                    dist[nx][ny] = steps + 1;
+                    q.push({{nx,ny}, steps+1});
                 }
             }
-            
-             if(it.first.first+1<x){
-                if(visited[it.first.first+1][it.first.second]==0){
-                    q.push_back({{it.first.first+1,it.first.second},it.second+1});
-                    visited[it.first.first+1][it.first.second]=1;
-                    
-                }
-            }
-            
-            
-             if(it.first.second-1>=0){
-                if(visited[it.first.first][it.first.second-1]==0){
-                    q.push_back({{it.first.first,it.first.second-1},it.second+1});
-                     visited[it.first.first][it.first.second-1]=1;
-                    
-                }
-            }
-            
-            
-             if(it.first.second+1<y){
-                if(visited[it.first.first][it.first.second+1]==0){
-                    q.push_back({{it.first.first,it.first.second+1},it.second+1});
-                    visited[it.first.first][it.first.second+1]=1;
-                    
-                }
-            }
-            
         }
-        
-        return final;
+        return dist;
     }
 };
