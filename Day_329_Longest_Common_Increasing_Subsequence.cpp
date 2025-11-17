d0b2f4b27f6084d517da8e1584f4cb024c8579ad
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-11-17 23:39:33 +0530

    feat: implement kLengthApart function to check if all 1's are at least k indices apart in the array

--- Day_329_Longest_Common_Increasing_Subsequence.cpp
+++ Day_329_Longest_Common_Increasing_Subsequence.cpp
@@ -11,7 +11,31 @@ using namespace std;
 // 4. Return the maximum value in the DP array as the result.
 
 // Time Complexity: O(n * m) where n and m are the sizes of arrays a and b respectively.
-// Space Complexity: O(m) where m is the size of array b.
+// Space Complexity: O(m) where m is the size of array b.class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+
+        int n = nums.size();
+        int pre_index;
+        bool f = true;
+
+        for (int i = 0; i < n; i++) {
+            if (f && nums[i] == 1) {
+                f = false;
+                pre_index = i;
+                continue;
+            }
+            if (nums[i] == 1) {
+                if (i - pre_index - 1 >= k)
+                    pre_index = i;
+                else
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
 
 class Solution {
   public:
