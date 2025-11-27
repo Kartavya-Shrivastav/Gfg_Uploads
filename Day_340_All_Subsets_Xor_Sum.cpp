65f8599c2de69c6b0fa3fa390059dc15b973f978
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-11-27 09:12:08 +0530

    feat: implement subsetXORSum function to calculate XOR sum of all subsets in Day_340_All_Subsets_Xor_Sum.cpp

---
+++ Day_340_All_Subsets_Xor_Sum.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int subsetXORSum(vector<int>& arr) {
+        int n=arr.size();
+        int s=0;
+        for(auto it:arr){
+            s|=it;
+        }
+        return s*pow(2,n-1);
+    }
+};
