e9da2672b6b42ad417d3a102f228e7bc5b30d9a0
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2026-04-03 20:44:28 +0530

    feat: add detailed documentation and structure for largestBst function to find the largest binary search tree in a binary tree

--- Day_448_Largest BST.cpp
+++ Day_448_Largest BST.cpp
@@ -1,3 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Problem: Given a binary tree, find the size of the largest binary search tree (BST) that is a subtree of the given tree. A BST is defined as a binary tree in which for every node, the value of all nodes in its left subtree is less than the node's value, and the value of all nodes in its right subtree is greater than the node's value.
+Approach: To solve this problem, we can use a depth-first search (DFS) approach. We will traverse the tree and for each node, we will check if the subtree rooted at that node is a BST. If it is a BST, we will calculate its size and keep track of the maximum size found so far.
+
+Steps:
+1. Perform a DFS traversal of the tree.
+2. For each node, check if the subtree rooted at that node is a BST by comparing the values of the left and right subtrees with the current node's value.
+3. If the subtree is a BST, calculate its size and update the maximum size found so far.
+4. If the subtree is not a BST, return a size of 0 for that subtree.
+5. Continue the traversal until all nodes have been visited.
+
+Time Complexity: O(N) where N is the number of nodes in the tree, as we visit each node once during the DFS traversal.
+Space Complexity: O(H) where H is the height of the tree, due to the recursive call stack during the DFS traversal. In the worst case (skewed tree), this can be O(N).
+*/
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
 
 class Solution {
   public:
