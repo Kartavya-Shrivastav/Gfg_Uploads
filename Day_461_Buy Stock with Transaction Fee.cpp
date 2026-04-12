51c5e8a29a92bd12dd91b96d88ad1a0a8670a37c
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2026-04-12 21:52:00 +0530

    feat: implement maxProfit function to calculate maximum profit with transaction fee

---
+++ Day_461_Buy Stock with Transaction Fee.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int maxProfit(vector<int>& arr, int k) {
+        int n = arr.size();
+        if (n == 0) return 0;
+
+        long long hold = -arr[0]; 
+        long long sell = 0;
+
+        for (int i = 1; i < n; i++) {
+            long long prev_hold = hold;
+        
+            hold = max(hold, sell - arr[i]);
+        
+            sell = max(sell, prev_hold + arr[i] - k);
+        }
+        return (int)sell;
+    }
+};
